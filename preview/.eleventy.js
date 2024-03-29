c8b7cb6355390fe40c4f2e492c69431ed5770b29
codecalm <codecalm@gmail.com>
2024-03-29 17:34:44 +0100

    optimize icons

--- preview/.eleventy.js
+++ preview/.eleventy.js
@@ -7,11 +7,19 @@ module.exports = function (eleventyConfig) {
 
       let c = {}
       Object.entries(countries).forEach(([key, country]) => {
+         // get size of the svg file
+         country.size = require('fs').statSync(`flags/${key}.svg`).size
+
          if (!c[country.category]) {
             c[country.category] = {}
          }
 
          c[country.category][key] = country
+      })
+
+      // sort by size
+      Object.entries(c).forEach(([category, countries]) => {
+         c[category] = Object.fromEntries(Object.entries(countries).sort((a, b) => a[1].size - b[1].size).reverse())
       })
 
       return c
